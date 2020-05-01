dd5c4fc23c339f8521e7c8e47def41919abed9cf
RAMNATH SHARMA <ramsharma.axel@gmail.com>
2020-05-02 00:16:40 +0530

    Logout functionality added

--- accounts/views.py
+++ accounts/views.py
@@ -4,8 +4,10 @@ from django.contrib import auth
 
 def signup(request):
     if request.method == 'POST':
-        #user has info and wants account now!
+        #user has info and wants to create an account now!
+        #check if both passwords match or not
         if request.POST['password1'] == request.POST['password2']:
+            #Check if user already exist or not
             try:
                 user = User.objects.get(username=request.POST['username'])
                 return render(request, 'accounts/signup.html', {'error':'Username has already been taken.'})
@@ -13,7 +15,6 @@ def signup(request):
                 user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
                 auth.login(request, user)
                 return redirect('home')
-
         else:
             return render(request, 'accounts/signup.html', {'error':'Password must match.'})
 
@@ -21,7 +22,18 @@ def signup(request):
         #user wants to enter info
         return render(request, 'accounts/signup.html')
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method=='POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'error':'Username or password is not correct'})
+    else:
+        return render(request, 'accounts/login.html')
+
 def logout(request):
     # TODO need to route to home page
-    return render(request, 'accounts/signup.html')
+    if request.method=='POST':
+        auth.logout(request)
+        return redirect('home')
