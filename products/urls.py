a65b9897e0cf79c69037b829c08d9ab398de7c10
RAMNATH SHARMA <ramsharma.axel@gmail.com>
2020-05-10 23:43:03 +0530

    Added Upvote and Home page

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('create/', views.create, name='create'),
-    # path('login/', views.login, name='login'),
-    # path('logout/', views.logout, name='logout'),
-
+    path('<int:product_id>', views.detail, name='detail'),
+    path('<int:product_id>/upvote', views.upvote, name='upvote'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Product
 from django.utils import timezone
 
 def home(request):
-    return render(request, 'products/home.html')
+    products = Product.objects
+    return render(request, 'products/home.html', {'products':products})
 
 @login_required
 def create(request):
@@ -23,6 +24,18 @@ def create(request):
             product.pub_date = timezone.datetime.now()
             product.hunter = request.user
             product.save()
-            return redirect('home')
+            return redirect('/products/' + str(product.id))
     else:
         return render(request, 'products/create.html')
+
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'products/detail.html', {'product': product})
+
+@login_required
+def upvote(request, product_id):
+    if request.method == 'POST':
+        product = get_object_or_404(Product, pk=product_id)
+        product.votes_total += 1
+        product.save()
+        return redirect('/products/' + str(product.id))
